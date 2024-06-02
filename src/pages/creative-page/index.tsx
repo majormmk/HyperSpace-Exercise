12104c5c238e9655e5b56478a07cd6ae75790d5c
majormmk <inc.mohd@gmail.com>
2024-06-03 01:30:35 +0400

    fixing ts error

--- src/pages/creative-page/index.tsx
+++ src/pages/creative-page/index.tsx
@@ -4,7 +4,7 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random";
 import EarthCanvas from "./Earth";
 
-const Stars = (props) => {
+const Stars = () => {
   const ref = useRef();
   const sphere = random.inSphere(new Float32Array(5001), { radius: 1.2 });
 
@@ -15,7 +15,7 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
         <PointMaterial
           transparent
           color="#f272c8"
