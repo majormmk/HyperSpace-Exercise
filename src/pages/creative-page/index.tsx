5d4f77e13cd0a66f3eb8d43fb0a490d2e97ad5b9
majormmk <inc.mohd@gmail.com>
2024-06-03 01:39:24 +0400

    comitting ts errors fixed

--- src/pages/creative-page/index.tsx
+++ src/pages/creative-page/index.tsx
@@ -1,21 +1,28 @@
-import { useRef, Suspense } from "react";
+import { useRef, Suspense, RefObject } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random";
 import EarthCanvas from "./Earth";
 
-const Stars = () => {
-  const ref = useRef();
+interface MyObjectType {
+  rotation: { x: number; y: number };
+  // Other properties if any
+}
+
+const Stars = (props: any) => {
+  const ref: RefObject<MyObjectType> = useRef(null);
   const sphere = random.inSphere(new Float32Array(5001), { radius: 1.2 });
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 15;
-    ref.current.rotation.y -= delta / 10;
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 15;
+      ref.current.rotation.y -= delta / 10;
+    }
   });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
