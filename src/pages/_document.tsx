762a1a6b6e8cbdf9c1d6bb410fbd4ac378599453
majormmk <inc.mohd@gmail.com>
2024-06-03 01:23:48 +0400

    comitting file cleaning

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>HyperSpace-Exercise</title>
       <Head />
       <body>
         <Main />
--- src/pages/creative-page/Earth.tsx
+++ src/pages/creative-page/Earth.tsx
@@ -2,8 +2,6 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from "../Loader";
-
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
